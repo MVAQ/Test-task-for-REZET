a9a5d794f4245ab64f9e00dab2edbfb96bb2c560
Владимир <41942471+MVAQ@users.noreply.github.com>
2021-10-30 21:22:15 +0300

    Add files via upload

--- libFuncTask1_2_3.php
+++ libFuncTask1_2_3.php
@@ -1,21 +1,27 @@
 <?php
-$data = date('l jS \of F Y h:i:s A');
-$sizeArrey1 = $_REQUEST['sizeArrey1'];
-echo $sizeArrey1;
-echo '<br/>';
-//********************************************************************
-$arrTask1 = [0];
-//********************************************************************
-$arrTask2 = [0][0];
-//********************************************************************
-$arrTask3String = [["Hello", "world"],
-    ["Brad", "Came", "to", "dinner", "with", "us"],
-    ["He", "loves", "tacos"]];
-$arrTask3FormatString = ["LEFT", "RIGHT", "LEFT",];
-$intTask3LimitVar = 16;
-//********************************************************************
 
-function getArreyOfTask1($x)
+$date = date('l jS \of F Y h:i:s A');
+echo $date,'<br/>';
+
+$sizeArray1=9;
+
+$arrTask1 = [0];
+
+$arrTask2 = [0][0];
+
+$arrTask3String = [['Hello', 'world'],
+    ['Brad', 'Came', 'to', 'dinner', 'with', 'us'],
+    ['He', 'loves', 'tacos']];
+
+$arrTask3FormatString = ['LEFT', 'RIGHT', 'LEFT'];
+
+$intTask3LimitVar = 16;
+
+/*
+ * @param int
+ * @return array
+ */
+function getArrayOfTask1($x)
 {
     $arrTask1 = [$x];
     for ($j = 0; $j < $x; $j++) {
@@ -24,9 +30,14 @@ function getArreyOfTask1($x)
     return $arrTask1;
 }
 
-function getArreyOfTask2($x)
+/*
+ * @param int
+ * @return arrey
+ */
+function getArrayOfTask2($x)
 {
     $arrTask2 = [3][$x];
+
     for ($i = 0; $i < 3; $i++) {
         for ($j = 0; $j < $x; $j++) {
             $arrTask2[$i][$j] = random_int(1, 9);
@@ -35,94 +46,185 @@ function getArreyOfTask2($x)
     return $arrTask2;
 }
 
-function сomparisonOfJobValues($x)
+/*
+ * @param array
+ * @return bool
+ */
+function сomparisonOfJobValuesTask1($x)
 {
     $boolResultTask1 = [];
     $сontrolVariable = 0;
-    var_dump($x);
-    echo '  <br/>';
+
     for ($i = 0; $i < count($x); $i++) {
-//         var_dump($boolResultTask1);
-//        echo '  <br/>';
-//        echo $boolResultTask1[$сontrolVariable];
+
         if ($x[$i] < $x[$i - 1] && $x[$i] < $x[$i + 1]) {
-            $boolResultTask1[$сontrolVariable] = '1';
+            $boolResultTask1[$сontrolVariable] = 1;
             $сontrolVariable++;
         } elseif ($x[$i] > $x[$i - 1] && $x[$i] > $x[$i + 1]) {
-            $boolResultTask1[$сontrolVariable] = '0';
+            $boolResultTask1[$сontrolVariable] = 0;
             $сontrolVariable++;
-        } else
+        } else {
             continue;
+        }
     }
     return $boolResultTask1;
 }
-
-function enumerationOfValuesTask2($arrTask2, $sizeArrey1)
+/*
+ * @param array
+ * @return woid
+ */
+function showResultTask1($boolResultTask1)
 {
-//                  var_dump($x);
-    echo '<br/>';
-//                                           echo $x+'<br/>';
+    echo 'Результат работы функции в задании №1','<br>';
+
+    foreach ($boolResultTask1 as  $element)
+    {
+        echo $element;
+        echo',';
+    }
+    echo'<br>';
+    echo str_repeat('*', 30), '<br>';
+}
+/*
+ * @param arrey int
+ * @return string
+ */
+function enumerationOfValuesTask2($arrTask2, $sizeArray1)
+{
+
+    $resultArrayTask2=[];
+    $y = 0;
+
     for ($i = 0; $i < count($arrTask2); $i++) {
+
         for ($j = 0; $j < count($arrTask2[$i]); $j++) {
             echo '    ', $arrTask2[$i][$j];
         }
         echo '<br/>';
     }
-//    echo '<br/><br/>';
-    $y = 0;
+
+
+
+
     do {
-        $tempArrey = [];
+        $tempArray = [];
         echo '<br/>';
+
         for ($i = 0; $i < count($arrTask2); $i++) {
+
             for ($j = $y; $j < $y + 3; $j++) {
                 echo '    ', $arrTask2[$i][$j];
-                array_push($tempArrey, $arrTask2[$i][$j]);
+                array_push($tempArray, $arrTask2[$i][$j]);
             }
             echo '<br/>';
         }
-        $tempArrey = array_unique($tempArrey, SORT_NUMERIC);
-//        var_dump($tempArrey);
+
+        $tempArray = array_unique($tempArray, SORT_NUMERIC);
         $y++;
-        if (count($tempArrey) === 9)
+
+        if (count($tempArray) === 9) {
             echo ' 1 This arrey сontains all nambers ', '<br/>';
-        else
+            $resultArrayTask2[$y]='true';
+        } else {
             echo ' 0 This arrey is not сontains all nambers  ', '<br/>';
-    } while ($y != $sizeArrey1);
-}
-
-function showResultTask3($arrTask3String, $arrTask3FormatString, $intTask3LimitVar)
-{
-    var_dump($arrTask3String);
-    echo str_repeat("*", $intTask3LimitVar), "*<br>";
-    foreach ($arrTask3String as $key => $str) {
-        echo "*", implode($str," " ), "*<br>";
-        $str=implode($str," " );
-//        echo strlen($str),"*<br>";// убрать в будущем
-
-
-
-        if(strlen($str)>$intTask3LimitVar){
-            $subStringArr=explode(' ',$str,$intTask3LimitVar);
-
-            echo $str ,"<br>";
-
-var_dump($subStringArr);
-
+            $resultArrayTask2[$y]='false';
         }
-//        foreach ($str as $key => $str1)
 
-//            echo "*", print($key), printf("[%s]\n", $str), "*<br>";
+    } while ($y != $sizeArray1);
+
+    return $resultArrayTask2;
+}
+/*
+ * @param array
+ * @return woid
+ */
+function showResultTask2($resultArrayTask2)
+{
+    echo '<br>';
+    echo 'Результат работы функции в задании №2','<br>';
+
+    foreach ($resultArrayTask2 as $element)
+    {
+        echo $element;
+        echo ',';
     }
-    echo str_repeat("*", 16), "<br>";
+    echo '<br>';
+
+}
+
+/*
+ * @param array, array, int
+ * @return array
+ */
+function formatArrayStringTask3($arrTask3String, $arrTask3FormatString, $intTask3LimitVar)
+{
+    foreach ($arrTask3String as $key => $str) {
+        $str = implode($str, ' ');
+
+        if (strlen($str) > $intTask3LimitVar) {
+            $subStringArr = explode(' ', $str, $intTask3LimitVar);
+            $str = implode(array_slice($subStringArr, 0, count($subStringArr) / 2, false), ' ');
+            $str1 = implode(array_slice($subStringArr, count($subStringArr) / 2, count($subStringArr) / 2, false), ' ');
+            array_push($arrayAfterFormatting, $str);
+            array_push($arrayAfterFormatting, $str1);
+        }
+        else {
+            $arrayAfterFormatting[] = $str;
+        }
+    }
+    $contrVar = 0;
+    do{
+        if (strlen($arrTask3FormatString[$contrVar]) == 5) {
+            $arrayAfterFormatting[$contrVar] = str_pad($arrayAfterFormatting[$contrVar], $intTask3LimitVar, ' ', STR_PAD_LEFT);
+           $arrayAfterFormatting[$contrVar] = str_pad($arrayAfterFormatting[$contrVar], $intTask3LimitVar+2, '"*', STR_PAD_LEFT);
+            $arrayAfterFormatting[$contrVar] = str_pad($arrayAfterFormatting[$contrVar], $intTask3LimitVar+5, '*",' );
+            ++$contrVar;
+            $arrayAfterFormatting[$contrVar] = str_pad($arrayAfterFormatting[$contrVar], $intTask3LimitVar, ' ', STR_PAD_LEFT);
+           $arrayAfterFormatting[$contrVar] = str_pad($arrayAfterFormatting[$contrVar], $intTask3LimitVar+2, '"*', STR_PAD_LEFT);
+            $arrayAfterFormatting[$contrVar] = str_pad($arrayAfterFormatting[$contrVar], $intTask3LimitVar+5, '*",' );
+        }
+        else {
+            $arrayAfterFormatting[$contrVar] = str_pad($arrayAfterFormatting[$contrVar], $intTask3LimitVar, ' ' );
+            $arrayAfterFormatting[$contrVar] = str_pad($arrayAfterFormatting[$contrVar], $intTask3LimitVar+2, '"*', STR_PAD_LEFT);
+           $arrayAfterFormatting[$contrVar] = str_pad($arrayAfterFormatting[$contrVar], $intTask3LimitVar+5, '*",' );
+            ++$contrVar;
+        }
+
+    }while($contrVar!=4);
+
+    return $arrayAfterFormatting;
+}
+/*
+ * @param array
+ * @return woid
+ */
+function showResultTask3($arrayAfterFormatting)
+{
+    echo 'Результат работы функции в задании №3','<br>';
+    var_dump($arrayAfterFormatting);
+    echo '[','<br>';
+    echo str_repeat('*', 16), '<br>';
+
+    foreach ($arrayAfterFormatting as $element)
+    {
+        print_r ($element);
+       echo '<br>';
+    }
+    echo str_repeat('*', 16), '<br>';
+    echo ']';
 }
 
 
-//getArreyOfTask1($sizeArrey1);
-//getArreyOfTask2($sizeArrey1);
-//$boolArrey = сomparisonOfJobValues(getArreyOfTask1($sizeArrey1));
-//var_dump($boolArrey);
-//enumerationOfValuesTask2(getArreyOfTask2($sizeArrey1), $sizeArrey1);
 
-showResultTask3($arrTask3String, $arrTask3FormatString, $intTask3LimitVar);
+getArrayOfTask1($sizeArray1);
+getArrayOfTask2($sizeArray1);
+
+$boolArrayTask1 = сomparisonOfJobValuesTask1(getArrayOfTask1($sizeArray1));
+showResultTask1($boolArrayTask1);
+
+$boolArrayTask2 = enumerationOfValuesTask2(getArrayOfTask2($sizeArray1), $sizeArray1);
+showResultTask2($boolArrayTask2);
+
+showResultTask3(formatArrayStringTask3($arrTask3String, $arrTask3FormatString, $intTask3LimitVar));
 
 ?>
